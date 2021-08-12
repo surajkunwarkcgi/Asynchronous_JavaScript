3f1a3c32c26d34363c78475e40b1fb18c3c91b87
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2021-08-12 11:22:14 +0900

    Building a simple promise and promisifying it

--- script.js
+++ script.js
@@ -197,4 +197,32 @@ const whereAmI = function (lat, lng) {
     .catch(err => console.error(`${err.message}`));
 };
 
-whereAmI(52.508, 13.381);
+//whereAmI(52.508, 13.381);
+
+//Building a simple promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happenning...');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('you won the money.');
+    } else {
+      reject('you lost the money.');
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+//Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
